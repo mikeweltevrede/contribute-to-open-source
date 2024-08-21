028da64faf45f59859696d6ba1e4f205b477e7e7
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2024-08-21 15:12:40 +0200

    Add support for labels

--- contribute_to_open_source/searchers/issues.py
+++ contribute_to_open_source/searchers/issues.py
@@ -8,11 +8,14 @@ class QueryParameters:
     """Parameters for GitHub API search."""
 
     languages: list[str] = field(default_factory=list)
+    labels: list[str] = field(default_factory=list)
 
     def __post_init__(self) -> None:
         """Post-initialization, doing data validation."""
         if len(self.languages) > 1:
             raise NotImplementedError("Currently no support for multiple languages")
+        if len(self.labels) > 1:
+            raise NotImplementedError("Currently no support for multiple labels")
 
 
 def generate_github_api_query(query_params: QueryParameters) -> str:
@@ -25,5 +28,6 @@ def generate_github_api_query(query_params: QueryParameters) -> str:
     """
     query = ""
     query += " OR ".join(f'language:"{lang}"' for lang in query_params.languages)
+    query += " OR ".join(f'label:"{label}"' for label in query_params.labels)
 
     return query
--- tests/unit/searchers/issues_test.py
+++ tests/unit/searchers/issues_test.py
@@ -21,3 +21,20 @@ class TestGenerateGithubApiQuery:
     def test_when_languages_not_provided_do_not_add_to_query(self):
         params = issues.QueryParameters()
         assert "language:" not in issues.generate_github_api_query(params)
+
+    def test_labels_for_single_element_is_added_to_query(self):
+        expected = 'label:"good-first-issue"'
+        params = issues.QueryParameters(labels=["good-first-issue"])
+
+        assert issues.generate_github_api_query(params) == expected
+
+    @pytest.mark.xfail(reason="Currently no support for multiple labels")
+    def test_labels_for_multiple_elements_is_added_to_query(self):
+        expected = 'label:"good-first-issue" OR label:"help wanted"'
+        params = issues.QueryParameters(languages=["good-first-issue", "help wanted"])
+
+        assert issues.generate_github_api_query(params) == expected
+
+    def test_when_labels_not_provided_do_not_add_to_query(self):
+        params = issues.QueryParameters()
+        assert "label:" not in issues.generate_github_api_query(params)
