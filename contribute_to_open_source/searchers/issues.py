af9ff2138180c91c54469b98dcd34821b7e41f6d
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2024-09-10 21:11:43 +0200

    ADD: Code to search GitHub issues (#11)

---
+++ contribute_to_open_source/searchers/issues.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Any
+
+import requests
+
+
+@dataclass
+class QueryParameters:
+    """Parameters for GitHub API search."""
+
+    languages: list[str] = field(default_factory=list)
+    labels: list[str] = field(default_factory=list)
+    states: list[str] = field(default_factory=list)
+
+    def __post_init__(self) -> None:
+        """Post-initialization, doing data validation."""
+        # TODO(mike): Currently do not support more than one element in the fields. However, for backwards
+        #  compatibility's sake, we do define the fields as a list and do validation in post-init.
+        # https://github.com/mikeweltevrede/contribute-to-open-source/issues/9
+        multi_elt_fields = [attr for attr, value in self.__dict__.items() if len(value) > 1]
+
+        if multi_elt_fields:
+            raise NotImplementedError(f"Currently no support for multiple {','.join(multi_elt_fields)}")
+
+
+def generate_github_api_query(query_params: QueryParameters) -> str:
+    """Generate query with filters to retrieve issues with.
+
+    Filters should match those for the GitHub API.
+
+    :param query_params: Query parameters.
+    :return: Query with filters.
+    """
+    query = ""
+    query += " OR ".join(f'language:"{lang}"' for lang in query_params.languages)
+    query += " OR ".join(f'label:"{label}"' for label in query_params.labels)
+    query += " OR ".join(f'state:"{state}"' for state in query_params.states)
+
+    return query
+
+
+def search_issues(query_params: QueryParameters) -> dict[str, Any]:
+    """Search GitHub issues according to the query parameters.
+
+    :param query_params: Query parameters to GitHub's issue API.
+    :return: JSON return value from the request to the issue API.
+    """
+    github_api_url = "https://api.github.com/search/issues"
+
+    # TODO(mike): Authenticate when using the GitHub API
+    # https://github.com/mikeweltevrede/contribute-to-open-source/issues/8
+    headers = {"Accept": "application/vnd.github.v3+json"}
+
+    params: dict[str, str | int] = {
+        "q": generate_github_api_query(query_params=query_params),
+        "sort": "created",
+        "order": "desc",
+        # TODO(mike): Un-hardcode these, probably in a pagination dataclass or general params dataclass
+        # https://github.com/mikeweltevrede/contribute-to-open-source/issues/7
+        "page": 1,
+        "per_page": 100,
+    }
+
+    github_response = requests.get(github_api_url, params=params, headers=headers, timeout=60)
+    return github_response.json()
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    query_params = QueryParameters(languages=["python"], labels=["good first issue"], states=["open"])
+    response = search_issues(query_params=query_params)
+
+    pprint(response)
---
+++ tests/unit/searchers/issues_test.py
@@ -0,0 +1,115 @@
+from __future__ import annotations
+
+from unittest import mock
+
+import pytest
+from contribute_to_open_source.searchers import issues
+
+
+class TestQueryParameters:
+    def test_when_languages_not_provided_set_to_empty_list(self):
+        assert issues.QueryParameters().languages == []
+
+    def test_when_labels_not_provided_set_to_empty_list(self):
+        assert issues.QueryParameters().labels == []
+
+    def test_when_states_not_provided_set_to_empty_list(self):
+        assert issues.QueryParameters().states == []
+
+    def test_raises_NotImplementedError_if_more_than_one_elt_for_languages(self):
+        with pytest.raises(NotImplementedError, match="Currently no support for multiple languages"):
+            issues.QueryParameters(languages=["python", "java"])
+
+    def test_raises_NotImplementedError_if_more_than_one_elt_for_labels(self):
+        with pytest.raises(NotImplementedError, match="Currently no support for multiple labels"):
+            issues.QueryParameters(labels=["good-first-issue", "help wanted"])
+
+    def test_raises_NotImplementedError_if_more_than_one_elt_for_states(self):
+        with pytest.raises(NotImplementedError, match="Currently no support for multiple states"):
+            issues.QueryParameters(states=["open", "closed"])
+
+    def test_raises_NotImplementedError_if_more_than_one_elt_for_languages_and_labels(self):
+        with pytest.raises(NotImplementedError, match="Currently no support for multiple languages,labels"):
+            issues.QueryParameters(languages=["python", "java"], labels=["good-first-issue", "help wanted"])
+
+
+class TestGenerateGithubApiQuery:
+    def test_languages_for_single_element_is_added_to_query(self):
+        expected = 'language:"python"'
+        params = issues.QueryParameters(languages=["python"])
+
+        assert issues.generate_github_api_query(params) == expected
+
+    @pytest.mark.xfail(reason="Currently no support for multiple languages")
+    def test_languages_for_multiple_elements_is_added_to_query(self):
+        expected = 'language:"python" OR language:"java"'
+        params = issues.QueryParameters(languages=["python", "java"])
+
+        assert issues.generate_github_api_query(params) == expected
+
+    def test_when_languages_not_provided_do_not_add_to_query(self):
+        params = issues.QueryParameters()
+        assert "language:" not in issues.generate_github_api_query(params)
+
+    def test_labels_for_single_element_is_added_to_query(self):
+        expected = 'label:"good-first-issue"'
+        params = issues.QueryParameters(labels=["good-first-issue"])
+
+        assert issues.generate_github_api_query(params) == expected
+
+    @pytest.mark.xfail(reason="Currently no support for multiple labels")
+    def test_labels_for_multiple_elements_is_added_to_query(self):
+        expected = 'label:"good-first-issue" OR label:"help wanted"'
+        params = issues.QueryParameters(languages=["good-first-issue", "help wanted"])
+
+        assert issues.generate_github_api_query(params) == expected
+
+    def test_when_labels_not_provided_do_not_add_to_query(self):
+        params = issues.QueryParameters()
+        assert "label:" not in issues.generate_github_api_query(params)
+
+    def test_states_for_single_element_is_added_to_query(self):
+        expected = 'state:"open"'
+        params = issues.QueryParameters(states=["open"])
+
+        assert issues.generate_github_api_query(params) == expected
+
+    @pytest.mark.xfail(reason="Currently no support for multiple states")
+    def test_states_for_multiple_elements_is_added_to_query(self):
+        expected = 'state:"open" OR state:"closed"'
+        params = issues.QueryParameters(states=["open", "closed"])
+
+        assert issues.generate_github_api_query(params) == expected
+
+    def test_when_states_not_provided_do_not_add_to_query(self):
+        params = issues.QueryParameters()
+        assert "state:" not in issues.generate_github_api_query(params)
+
+
+@mock.patch("contribute_to_open_source.searchers.issues.requests.get")
+class TestSearchIssues:
+    def test_request_is_made_to_github_api(self, mock_get):
+        github_api_url = "https://api.github.com/search/issues"
+
+        issues.search_issues(query_params=issues.QueryParameters())
+
+        mock_get.assert_called_once_with(github_api_url, params=mock.ANY, headers=mock.ANY, timeout=mock.ANY)
+
+    def test_response_is_accepted_to_be_from_github_vendor(self, mock_get):
+        issues.search_issues(query_params=issues.QueryParameters())
+
+        actual = mock_get.call_args.kwargs["headers"]["Accept"]
+
+        assert "vnd.github" in actual
+
+    @mock.patch("contribute_to_open_source.searchers.issues.generate_github_api_query")
+    def test_params_query_is_constructed_with_generate_github_api_query_and_input_query_params(
+        self, mock_generate_github_api_query, mock_get
+    ):
+        params = issues.QueryParameters()
+        issues.search_issues(query_params=params)
+
+        actual_get_params = mock_get.call_args.kwargs["params"]
+
+        mock_generate_github_api_query.assert_called_once_with(query_params=params)
+        assert actual_get_params["q"] == mock_generate_github_api_query()
