2e83a9ee7c8a659e551cb9646b64e9d7b99c6f59
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2024-09-13 20:13:45 +0200

    Add token to headers

--- contribute_to_open_source/api_helpers/headers.py
+++ contribute_to_open_source/api_helpers/headers.py
@@ -1,13 +1,25 @@
 from __future__ import annotations
 
+import os
 
-def construct_github_header(accepted_format: str = "json") -> dict[str, str]:
+
+def construct_github_header(token: str | None = None, accepted_format: str = "json") -> dict[str, str]:
     """Construct GitHub API header.
 
     Construct the header for calls to the GitHub API, version 3. We specify the applications and the output format
     that are accepted.
 
+    :param token: GitHub Personal Access Token. If not provided, try to get from the "GITHUB_TOKEN" environment
+        variable. If it is not provided in there, do not add authorization to the header.
     :param accepted_format: Accepted output format. Defaults to "json".
     :return: Header for GitHub API call.
     """
-    return {"Accept": f"application/vnd.github.v3+{accepted_format}"}
+    headers = {"Accept": f"application/vnd.github.v3+{accepted_format}"}
+
+    token: str | None = token or os.getenv("GITHUB_TOKEN")
+    if token:
+        # If there is no token provided, and it is not found in the environment variable GITHUB_TOKEN, then
+        # we should not add the "Authorization" header to our request
+        headers["Authorization"] = token
+
+    return headers
--- tests/unit/api_helpers/headers_test.py
+++ tests/unit/api_helpers/headers_test.py
@@ -18,3 +18,24 @@ class TestConstructGithubHeader:
         actual = headers.construct_github_header()
 
         assert actual["Accept"].startswith("application/vnd.github.v3+")
+
+    def test_header_uses_authorization_from_token_argument_if_provided(self):
+        actual = headers.construct_github_header(token=(token := "test_token_123"))
+
+        assert actual["Authorization"] == token
+
+    def test_header_uses_authorization_from_env_var_GITHUB_TOKEN_if_token_argument_not_provided(self, monkeypatch):
+        monkeypatch.setenv("GITHUB_TOKEN", token := "test_token_123")
+
+        actual = headers.construct_github_header()
+
+        assert actual["Authorization"] == token
+
+    def test_when_token_not_provided_and_not_in_env_var_GITHUB_TOKEN_then_do_not_add_Authorization_to_header(
+        self, monkeypatch
+    ):
+        monkeypatch.delenv("GITHUB_TOKEN", raising=False)
+
+        actual = headers.construct_github_header()
+
+        assert "Authorization" not in actual
